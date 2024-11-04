e9901daa2342d55edbb3cf3025a8300d94c40ee1
ali-karkazan <ali.karkazan@icloud.com>
2024-11-04 19:59:36 +0400

    create Footer component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Header from './components/Header';
 import Nav from './components/Nav';
 import Main from './components/Main'
+import Footer from './components/Footer'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Nav />
       <Main />
+      <Footer />
     </>
   );
 }
