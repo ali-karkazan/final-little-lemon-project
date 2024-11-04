7b42179e8ec922fba10ed2a384368c59182a7c20
ali-karkazan <ali.karkazan@icloud.com>
2024-11-04 19:57:28 +0400

    Create the Footer Component

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from '../Asset/Logo .svg'
+const Footer = () => {
+    return (
+        <footer>
+            <p>&copy; 2024 Little Lemon. All rights reserved.</p>
+            <img src={logo} alt='logo' />
+        </footer>
+    )
+}
+
+export default Footer;
